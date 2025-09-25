31763c59a89342ecfb9bf7ad5a40f8253f5c4c96
안성해 <58692441+ansunghae@users.noreply.github.com>
2025-09-25 16:30:25 +0900

    Add files via upload

---
+++ 0925/CallByValue.java
@@ -0,0 +1,13 @@
+public class CallByValue {
+    public static void main(String[] args) {
+        int n = 10;
+
+        increase(n);
+
+        System.out.println(n);
+    }
+
+    public static void increase(int m) {
+        m++;
+    }
+}
